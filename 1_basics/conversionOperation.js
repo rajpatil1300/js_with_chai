19290a64af6eeb3b01a432b2ccc8f8522c24e0d1
rajpatil1300 <rajpatil8931@gmail.com>
2025-06-10 17:34:18 +0000

    operation add

--- 1_basics/conversionOperation.js
+++ 1_basics/conversionOperation.js
@@ -1,24 +1,35 @@
-let score = false
-console.log(typeof(score));
-console.log(typeof score);
+// let score = false
+// console.log(typeof(score));
+// console.log(typeof score);
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
-let loggedIn =""
+// let loggedIn =""
 
-let isLoggedIn = Boolean(loggedIn)
-console.log(isLoggedIn )
+// let isLoggedIn = Boolean(loggedIn)
+// console.log(isLoggedIn )
 
 // 1=> true ; 0=> false
 //"" => false;
 //"value" => true
 
 
-let number = 333
+// let number = 333
 
-let _Number = String(number)
-console.log(typeof _Number);
-console.log(Number);
+// let _Number = String(number)
+// console.log(typeof _Number);
+// console.log(Number);
 
+//*************** Operations ********************************************//
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(1 + 3);
+// console.log(1+"34");
+// console.log("3" + 3 + 3);
+// console.log(3 + 4 + "4");
+console.log(+true);
