bf44632f7bfe868d5838e42ec3ac980d1526722c
rajpatil1300 <rajpatil8931@gmail.com>
2025-06-10 16:41:02 +0000

    dataTypes

---
+++ 1_basics/data_types.js
@@ -0,0 +1,17 @@
+"use strct"; //treat all js code as new version
+
+//alert(3 + 3) // we are using nodejs,not browser
+
+console.log(3+3);
+console.log("raj");
+
+let name = "raj"
+let age = 23
+let isLoggedIn = true
+
+// null => standalone value
+//undefined => value is not assigned
+//symbol => unique
+
+//object 
+// {NULL is object in javascript}
--- 1_basics/test.js
+++ 1_basics/test.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+const accId = 1234
+let accEmail = "raj@123"
+var accPass = "11233"
+accCity = "Jaipur"
+
+// accId = 3
+accEmail = "raj@2233"
+accPass = "122222"
+accCity = "lahore"
+console.log(accId);
+/*
+donot use var {scope issue}
+*/
+console.table([accId,accEmail,accPass,accCity ]);
