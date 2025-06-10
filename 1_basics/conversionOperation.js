654985609238a3c8643e9965ba9eadc6f7fc1a84
rajpatil1300 <rajpatil8931@gmail.com>
2025-06-10 17:07:46 +0000

    conversion

---
+++ 1_basics/conversionOperation.js
@@ -0,0 +1,24 @@
+let score = false
+console.log(typeof(score));
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let loggedIn =""
+
+let isLoggedIn = Boolean(loggedIn)
+console.log(isLoggedIn )
+
+// 1=> true ; 0=> false
+//"" => false;
+//"value" => true
+
+
+let number = 333
+
+let _Number = String(number)
+console.log(typeof _Number);
+console.log(Number);
+
